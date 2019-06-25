35893d31b86296c839db9e72fa764739342766ba
Valentyna Boriak <valenkiri@gmail.com>
2019-06-25 12:42:41 +0300

    modified test_guest_can_go_to_login_page function

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
+from .pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
-    login_link = driver.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(driver, link)
+    page.open()
+    page.go_to_login_page()
